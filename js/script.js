0d3e53cae1dded72ef15c7c5ae6d183162ca3101
Jason Hunsaker <jasonmhunsaker@gmail.com>
2021-02-10 21:01:17 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,21 +8,55 @@ project 1 - A Random Quote Generator
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
- * `quotes` array 
+ * array of quotes to display
 ***/
-
-
+let quotes = [{
+  quote: "You miss 100% of the shots you dont take - Wayne Gretzkey",
+  source: "- Michael Scott: The Office",
+},
+{
+  quote: "The bee stings the bird & the bee's life is over",
+  source: "- Gary Hunsaker",
+},
+{
+  quote: "I Know",
+  source: "-Han Solo, Star Wars: A New Hope",
+},
+{
+  quote: "Aim low, reach your goals, and avoid disappointment.",
+  source: "-Scott Adams",
+},
+{
+  quote: "If it’s your job to eat a frog, it’s best to do it first thing in the morning. And If it’s your job to eat two frogs, it’s best to eat the biggest one first.",
+  source: "- Mark Twain",
+},
+{
+  quote: "I reject your reality and substitute my own",
+  source: "- Adam Savage"
+}];
 
 /***
  * `getRandomQuote` function
 ***/
+function getRandomQuote (array) {
+  // random number to pick quote to display
+  var whichQuote = Math.floor(Math.random() * (quotes.length));
+  for (let i = 0; i < array.length; i++ ){
+    var randomQuote = array[whichQuote];
+  }
+  return randomQuote;
+} 
 
 
 
 /***
  * `printQuote` function
 ***/
-
+function printQuote(){
+  var quotePrint = getRandomQuote(quotes);
+  var input = `<p calss='quote'>${quotePrint.quote}</p><h1 class='source'>${quotePrint.source}</h1>`;
+  document.querySelector("quote-box").innerhtml = input;
+}
 
 
 /***
